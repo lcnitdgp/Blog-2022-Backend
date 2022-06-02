b0f7a7b2d5b67c20384aed1c2b76e0b636582388
Architlall <archit10dgp@gmail.com>
2022-06-02 18:05:08 +0530

    passport

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const app = express();
 const url = 'mongodb://localhost:27017/blog';
 const connect = mongoose.connect(url);
 
+var usersRouter = require('./routes/user');
+const blogRouter = require('./routes/Blogs');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
@@ -27,8 +29,31 @@ connect.then((db) =>{
     console.log('connected to server');
 })
 
-
+app.use(passport.initialize());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+    });
+
+//Method Override
+app.use(methodOverride((req,res)=>{
+  if(req.body && typeof req.body==='object' && '_method' in req.body){
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
+app.use('/users', usersRouter);
+app.use('/blog',blogRouter);
+
+module.exports = app;
 
