1c31c82fdba72ffcf56b06fd13c249ef2cca68a6
docilefiasco <epshita.hs@gmail.com>
2022-08-12 19:29:46 +0530

    connected message form

--- routes/message.js
+++ routes/message.js
@@ -9,12 +9,23 @@ const msgRouter = express.Router();
 msgRouter.use(express.json());
 
 msgRouter.route('/')
-     .post(authenticate.verifyUser,  (req, res, next) => {
+     .post(  (req, res, next) => {
         Message.create(req.body).then((msg)=>{
             res.statusCode = 200;
             res.json(msg);
         }), (err) => next(err)
     .catch((err) => next(err));
 })
+
+msgRouter.route("/getmsg").get((req, res, next) => {
+Message.find({}).then((message)=>{
+    
+     res.statusCode = 200;
+     res.setHeader("Content-Type", "application/json");
+     res.json(message);
+
+}),
+    (err) => next(err).catch((err) => next(err));
+});
 
 module.exports = msgRouter;
