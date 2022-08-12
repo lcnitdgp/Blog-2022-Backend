7eea0dc8bdf9e4d0e0175552d18907700b0a79f2
Architlall <archit10dgp@gmail.com>
2022-08-12 16:41:48 +0530

    send user object to frontend

--- routes/User.js
+++ routes/User.js
@@ -59,7 +59,7 @@ userRouter.post('/login', passport.authenticate('local'), (req, res) => {
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, token: token, status: 'You are successfully logged in!'});
+  res.json({user, token: token, status: 'You are successfully logged in!'});
 });
   
    
