312214ff981deb8754ab1b1530853729d98700d6
Architlall <archit10dgp@gmail.com>
2022-10-05 12:08:56 +0530

    cors

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ const server = http.createServer(app);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+  });
 app.use(logger("dev"));
 app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({limit:'200mb', extended: true })); 
