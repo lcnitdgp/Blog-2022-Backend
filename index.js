e298f40ebfa39004e9f8e429cebb25bd07b21b01
Architlall <archit10dgp@gmail.com>
2022-10-05 11:08:18 +0530

    file limit changed

--- index.js
+++ index.js
@@ -33,7 +33,9 @@ const server = http.createServer(app);
 app.listen(port, () => console.log(`Server running on port ${port}`));
 app.use(cors());
 app.use(logger("dev"));
-app.use(bodyParser.json({ limit: "50mb" }));
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({limit:'200mb', extended: true })); 
+app.use(bodyParser.json({ limit: "200mb" }));
 app.use(
   bodyParser.urlencoded({
     limit: "50mb",
